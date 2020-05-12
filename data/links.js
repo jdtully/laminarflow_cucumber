49c0929a8efcb4aff5e083756e9337f1d2cbcec0
Jeff <jefftully@gmail.com>
2020-05-12 14:33:47 -0400

    adding links

--- data/links.js
+++ data/links.js
@@ -4,33 +4,36 @@ const links = {
       "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div.title > div",
     url: "/user",
   },
+  People: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(3) > div.active.title > div",
+    url: "/user",
+  },
+  Flow: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(5) > div.active.title > div",
+    url: "/user",
+  },
+  Timeline: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(7) > div.active.title > div",
+    url: "/user",
+  },
+  Programs: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div:nth-child(2) > b > a",
+    url: "/user/programs",
+  },
+  Products: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div:nth-child(3) > b > a",
+    url: "/user/products",
+  },
+  Features: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div:nth-child(4) > b > a",
+    url: "/user/features",
+  },
 };
-// const  = new link(
-//   document.querySelector(
-//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div.title > div"
-//   ),
-//   "/user"
-// );
 
-// const peopleMain = new link(
-//   document.querySelector(
-//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(3) > div.active.title > div"
-//   ),
-//   "/user"
-// );
-
-// const flowMain = new link(
-//   document.querySelector(
-//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(5) > div.active.title > div"
-//   ),
-//   "/user"
-// );
-
-// const timelineMain = new link(
-//   document.querySelector(
-//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(7) > div.active.title > div"
-//   ),
-//   "/user"
-// );
-
-exports.links = links;
+export { links };

595a6966d3e8deec2fff6631db714ea4244a211b
Jeff <jefftully@gmail.com>
2020-05-12 13:26:15 -0400

    Started links component

---
+++ data/links.js
@@ -0,0 +1,36 @@
+const links = {
+  Program: {
+    selector:
+      "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div.title > div",
+    url: "/user",
+  },
+};
+// const  = new link(
+//   document.querySelector(
+//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(1) > div.title > div"
+//   ),
+//   "/user"
+// );
+
+// const peopleMain = new link(
+//   document.querySelector(
+//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(3) > div.active.title > div"
+//   ),
+//   "/user"
+// );
+
+// const flowMain = new link(
+//   document.querySelector(
+//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(5) > div.active.title > div"
+//   ),
+//   "/user"
+// );
+
+// const timelineMain = new link(
+//   document.querySelector(
+//     "#root > div > main > div > div.ui.inverted.vertical.labeled.icon.ui.push.left.thin.visible.sidebar.menu > div > div:nth-child(7) > div.active.title > div"
+//   ),
+//   "/user"
+// );
+
+exports.links = links;

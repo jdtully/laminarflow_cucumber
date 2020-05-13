a978751fcf23a62a1d45b500190289b850d3c08f
Jeff <jefftully@gmail.com>
2020-05-13 10:22:38 -0400

    Added generic user clicks

--- steps/dashboard/when.js
+++ steps/dashboard/when.js
@@ -1,6 +1,11 @@
 import { When } from "cucumber";
+import links from "../../data/links";
 
 When(/^The user clicks "(.*)"$/, (userNameLink) => {
   $(userNameLink).waitForDisplayed(10000);
   $(userName).click();
+});
+When(/^The User Clicks "(.*)"/, (keyword) => {
+  link = links.links[keyword]["selector"];
+  $(link).click();
 });
